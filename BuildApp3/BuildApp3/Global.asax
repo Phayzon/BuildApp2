769da181b0b3a178a8b346fc578fff80072e7395
Phayzon <phayzon.trice@fragmob.com>
2015-01-28 12:23:28 -0800

    Changes

---
+++ BuildApp3/BuildApp3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BuildApp3.Global" Language="C#" %>
---
+++ BuildApp3/BuildApp3/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BuildApp3.Startup))]
+namespace BuildApp3
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
