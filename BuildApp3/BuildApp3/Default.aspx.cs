675129d30bcab10a24df446a2b8a4471efe2796e
Phayzon <phayzon.trice@fragmob.com>
2015-02-02 16:39:50 -0800

    Testing

--- BuildApp3/BuildApp3/Default.aspx.cs
+++ BuildApp3/BuildApp3/Default.aspx.cs
@@ -23,7 +23,7 @@ namespace BuildApp3
             string TestUser = string.Empty;
             string Testuser2 = string.Empty;
 
-            TestUser = System.Security.Principal.WindowsIdentity.GetCurrent().Name.ToUpper();
+            TestUser = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
 
 
 
